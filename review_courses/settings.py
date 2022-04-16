2d428029bd8b1880cfcaddd08918a5064e30cba2
Nicholas Kallem <nicholas.kallem@gmail.com>
2022-04-16 14:27:08 -0700

    Created initial model

--- review_courses/settings.py
+++ review_courses/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'review_my_courses.apps.ReviewMyCoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'review_my_courses.apps.ReviewMyCoursesConfig',
 ]
 
 MIDDLEWARE = [
--- review_my_courses/models.py
+++ review_my_courses/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import date
 
-# Create your models here.
+
+# Validator function for the year
+def validate_year(value):
+    if 2000 <= value <= date.today().year:
+        return value
+    else:
+        raise ValidationError("Year must be greater than or equal to 2000 and cannot exceed the current year.")
+
+
+class School(models.Model):
+    """The school that offers the course to be reviewed."""
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the school."""
+        return self.name
+
+
+class Course(models.Model):
+    """Represents a course that can be reviewed."""
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    course_code = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns a string representation of the course, including the course code and title."""
+        return self.course_code + ": " + self.title
+
+
+class Review(models.Model):
+    """Represents a review of a course."""
+
+    # Specify choices
+    TERM_CHOICES = [
+        ('FALL', 'Fall'),
+        ('SPRING', 'Spring'),
+        ('SUMMER', 'Summer'),
+    ]
+
+    RATING_CHOICES = [
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
+    ]
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    term = models.CharField(choices=TERM_CHOICES)
+    year = models.PositiveIntegerField(validators=[validate_year])
+    workload = models.PositiveIntegerField()
+    difficulty = models.PositiveIntegerField(choices=RATING_CHOICES)
+    rating = models.PositiveIntegerField(choices=RATING_CHOICES)
+    text = models.TextField
+    review_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns a string representation of the course, including the course code and title."""
+        return self.course_code + ": " + self.title
+
