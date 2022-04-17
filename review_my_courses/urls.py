d47cdbf5f43bac6157fd38232672b0bb82594df5
Nicholas Kallem <nicholas.kallem@gmail.com>
2022-04-17 14:54:21 -0700

    Added a base.html file and created the initial schools page

--- review_my_courses/urls.py
+++ review_my_courses/urls.py
@@ -8,4 +8,6 @@ app_name = 'review_my_courses'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all schools.
+    path('schools/', views.schools, name='schools'),
 ]
--- review_my_courses/views.py
+++ review_my_courses/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Course, School, Review
 
 
 def index(request):
     """The home page for Review My Courses."""
     return render(request, 'review_my_courses/index.html')
+
+
+def schools(request):
+    """Show all schools."""
+    schools = School.objects.order_by('name')
+    context = {'schools': schools}
+    return render(request, 'review_my_courses/schools.html', context)
