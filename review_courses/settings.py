252912abf8024abaddda7f5ceb6ab0cf65ff0264
nkallem <nicholas.kallem@gmail.com>
2022-04-22 11:06:43 -0700

    Added custom 404 and 500 error pages

--- review_courses/settings.py
+++ review_courses/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6xd*n&o0p9nu$_c@r346uk08junrac$xk#n6$y(u-z7apan^@r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -63,8 +63,7 @@ ROOT_URLCONF = 'review_courses.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
