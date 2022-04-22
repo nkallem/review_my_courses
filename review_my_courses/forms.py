d65e11f309fa8dabf47a52195db4f7be9ab5839b
Nicholas Kallem <nicholas.kallem@gmail.com>
2022-04-22 09:35:47 -0700

    Changed workload label

--- review_my_courses/forms.py
+++ review_my_courses/forms.py
@@ -24,5 +24,5 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['term', 'year', 'workload', 'difficulty', 'rating', 'text']
-        labels = {'text': 'Review', 'workload': 'Workload (hrs/week)'}
+        labels = {'text': 'Review', 'workload': 'Workload (hours/week)'}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
